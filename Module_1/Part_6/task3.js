05ae1924b32a7e9d1b96d870c82da881b999fd90
Ruslan2015 <r.dokudaev@yandex.ru>
2025-03-11 10:02:16 +0300

    Create task3.js

---
+++ Module_1/Part_6/task3.js
@@ -0,0 +1,21 @@
+const question_1 = 'JavaScript появился в 1995 году';
+const question_2 = 'Спецификация JavaScript называется ECMAScript';
+const question_3 = 'JavaScript был создан за 1 месяц';
+
+if (confirm(question_1 + '?')) {
+    alert('Верно!');
+} else {
+    alert(question_1 + '!');
+}
+
+if (confirm(question_2 + '?')) {
+    alert('Верно!');
+} else {
+    alert(question_2 + '!');
+}
+
+if (confirm(question_3 + '?')) {
+    alert('Верно!');
+} else {
+    alert(question_3 + '!');
+}
