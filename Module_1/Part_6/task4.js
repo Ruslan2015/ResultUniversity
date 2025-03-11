60a433f007add3183c2af93855a9ce3d8b9b0269
Ruslan2015 <r.dokudaev@yandex.ru>
2025-03-11 10:02:42 +0300

    Create task4.js

---
+++ Module_1/Part_6/task4.js
@@ -0,0 +1,28 @@
+for (let i = 0; i < 3; i += 1) {
+    let newStudent = prompt('Введите имя нового студента!');
+    if (newStudent) {
+      newStudent = newStudent.trim();
+      alert(`Добро пожаловать, ${newStudent}!`)
+    }
+}
+
+i = 0;
+while (i<3) {
+    let newStudent = prompt('Введите имя нового студента!');
+    if (newStudent) {
+      newStudent = newStudent.trim();
+      alert(`Добро пожаловать, ${newStudent}!`)
+    }
+    i++;
+}
+
+i=0;
+do {
+    let newStudent = prompt('Введите имя нового студента!');
+    if (newStudent) {
+      newStudent = newStudent.trim();
+      alert(`Добро пожаловать, ${newStudent}!`)
+    }
+    i++;
+} while (i < 3);
+
