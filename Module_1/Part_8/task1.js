1bb310a0fcf2a249c2538e75822c2daff9e2b184
Ruslan2015 <r.dokudaev@yandex.ru>
2025-03-12 09:06:00 +0300

    Update task1.js

--- Module_1/Part_8/task1.js
+++ Module_1/Part_8/task1.js
@@ -1,2 +1,14 @@
-//
-// Test GitHub
+function getName1(name) {
+    return `Имя равно ${name}`;
+}
+
+const getName2 = function(name) {
+    return `Имя равно ${name}`;
+}
+
+const getName3 = (name) => `Имя равно ${name}`;
+
+console.log(getName1('Ruslan'));
+console.log(getName2('Igor'));
+console.log(getName3('Alex'));
+
