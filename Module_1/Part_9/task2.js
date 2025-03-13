e9f9774ab9c73cae907effeb2c5f9d8d01b3bb60
Ruslan Dokudaev <r.dokudaev@mail.ru>
2025-03-13 22:13:49 +0200

    module 1 task 2

--- Module_1/Part_9/task2.js
+++ Module_1/Part_9/task2.js
@@ -1,1 +1,13 @@
-//
+function getSumOfSequence(number) {
+    let secArray = [];
+    for (i=1; i<number+1; i++) {
+        secArray.push(i);
+    }
+
+    let firstElemet = secArray[0];
+    let lastElement = secArray[secArray.length-1];
+    let sum = firstElemet + lastElement;
+    return sum;
+}
+
+console.log(getSumOfSequence(5));
